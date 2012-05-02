798754f6a8ccddae022d1aa6ce92a5c4b5c77144
ronan <ronan@eklaweb.com>
2012-05-02 04:25:16 +0200

    Fix: removing some old PhoneGap calls

--- Android/DateTimePicker/datePickerPlugin.js
+++ Android/DateTimePicker/datePickerPlugin.js
@@ -6,11 +6,11 @@ var DatePicker = function() {
 	
 };
 
-DatePicker.prototype.showDateOrTime = function(action,successCallback, failureCallback) {
+DatePicker.prototype.showDateOrTime = function(action,successCallback, failureCallback, args) {
 	// If a timestamp is passed, we use it as a default value for the date / time picker. Else, we use current date.
 	var args = typeof arguments[3] !== null ? arguments[3] : (new Date()).getTime();
 
-	return PhoneGap.exec( 
+	return cordova.exec(
 	successCallback,     // Success callback from the plugin
 	failureCallback,     // Error callback from the plugin
 	'DatePickerPlugin',  // Tell PhoneGap to run "DatePickerPlugin" Plugin
@@ -18,11 +18,7 @@ DatePicker.prototype.showDateOrTime = function(action,successCallback, failureCa
 	[args]);        		 // Passing list of args to the plugin: ajout du args
 };
 
-PhoneGap.addConstructor(function() {
+cordova.addConstructor(function() {
   //Register the javascript plugin with Cordova
-  PhoneGap.addPlugin('datePickerPlugin', new DatePicker());
-
-  //Register the native class of plugin with Cordova
-  PluginManager.addService("DatePickerPlugin",
-                       		 "org.apache.cordova.plugin.DatePickerPlugin");
+  cordova.addPlugin('datePickerPlugin', new DatePicker());
 });
