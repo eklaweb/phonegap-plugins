1a7b0065fb4640a64c9a9850e5455535b489cacd
ronan <ronan@eklaweb.com>
2012-05-02 16:39:44 +0200

    Fix: cleaned up comments and useless parameter

--- Android/DateTimePicker/datePickerPlugin.js
+++ Android/DateTimePicker/datePickerPlugin.js
@@ -6,16 +6,16 @@ var DatePicker = function() {
 	
 };
 
-DatePicker.prototype.showDateOrTime = function(action,successCallback, failureCallback, args) {
+DatePicker.prototype.showDateOrTime = function(action,successCallback, failureCallback) {
 	// If a timestamp is passed, we use it as a default value for the date / time picker. Else, we use current date.
 	var args = typeof arguments[3] !== null ? arguments[3] : (new Date()).getTime();
 
 	return cordova.exec(
 	successCallback,     // Success callback from the plugin
 	failureCallback,     // Error callback from the plugin
-	'DatePickerPlugin',  // Tell PhoneGap to run "DatePickerPlugin" Plugin
+	'DatePickerPlugin',  // Tell Cordova to run "DatePickerPlugin" Plugin
 	action,              // Tell plugin, which action we want to perform
-	[args]);        		 // Passing list of args to the plugin: ajout du args
+	[args]);        	 // Passing list of args to the plugin: ajout du args
 };
 
 cordova.addConstructor(function() {
